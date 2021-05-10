8b78eac78ffd2131104ddb4519cf5f972f018604
Leandro Donato <leacavs11@gmail.com>
2021-05-10 15:48:08 -0300

    Se agregar Navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import {
+    AppBar, 
+    Toolbar,
+    IconButton,
+    Typography
+  } from "@material-ui/core"
+  import MenuIcon from "@material-ui/icons/Menu"
+    
+const Navbar = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" color="inherit" aria-label="menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">News</Typography>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar
